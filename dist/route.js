7b6d04d8c5e0e25a2a9ad619a380c966238f4b9c
Tsutomu Kawamura <cognitom@gmail.com>
2015-09-06 23:32:46 +0900

    Generated files

--- dist/route.js
+++ dist/route.js
@@ -17,67 +17,230 @@
 
 var observable = require('riot-observable')
 
-var EVT = 'hashchange', win = window, loc = win.location, started = false, fns = observable(), current
-
-/**
- * Get hash part of current URL
- * @returns {string} hash string
- */
-function hash() {
-  return loc.href.split('#')[1] || '' // why not loc.hash.splice(1) ?
-}
+var RE_ORIGIN = /^.+?\/+[^\/]+/,
+  EVENT_LISTENER = 'EventListener',
+  REMOVE_EVENT_LISTENER = 'remove' + EVENT_LISTENER,
+  ADD_EVENT_LISTENER = 'add' + EVENT_LISTENER,
+  HAS_ATTRIBUTE = 'hasAttribute',
+  REPLACE = 'replace',
+  POPSTATE = 'popstate',
+  TRIGGER = 'trigger',
+  win = window,
+  doc = document,
+  loc = win.history.location || win.location, // see html5-history-api
+  prot = Router.prototype, // to minify more
+  clickEvent = doc && doc.ontouchstart ? 'touchstart' : 'click',
+  started = false,
+  central = observable(),
+  base, current, parser, secondParser
 
 /**
  * Default parser. You can replace it via router.parser method.
- * @param {string} path - current path
- * @returns {*} array or object as you like
+ * @param {string} path - current path (normalized)
+ * @returns {array} array
  */
-function parser(path) {
-  return path.split('/')
+function DEFAULT_PARSER(path) {
+  return path.split(/[/?#]/)
 }
 
-function emit(path) {
-  if (path.type) path = hash()
+/**
+ * Default parser (second). You can replace it via router.parser method.
+ * @param {string} path - current path (normalized)
+ * @param {string} filter - filter string (normalized)
+ * @returns {array} array
+ */
+function DEFAULT_SECOND_PARSER(path, filter) {
+  var re = new RegExp('^' + filter[REPLACE](/\*/g, '(\\w+)')[REPLACE](/\.\./, '.*') + '$')
+  if (args = path.match(re)) return args.slice(1)
+}
 
-  if (path != current) {
-    fns.trigger.apply(null, ['H'].concat(parser(path)))
+/**
+ * Router class
+ */
+function Router() {
+  this.$ = []
+  observable(this) // make it observable
+  central.on('stop', this.s.bind(this))
+  central.on('emit', this.e.bind(this))
+}
+
+function normalize(path) {
+  return path[REPLACE](/^\/|\/$/, '')
+}
+
+/**
+ * Get the part after domain name
+ * @param {string} href - fullpath
+ * @returns {string} path from root
+ */
+function getPathFromRoot(href) {
+  return (href || loc.href)[REPLACE](RE_ORIGIN, '')
+}
+
+/**
+ * Get the part after base
+ * @param {string} href - fullpath
+ * @returns {string} path from base
+ */
+function getPathFromBase(href) {
+  return base[0] == '#'
+    ? (href || loc.href).split(base)[1] || ''
+    : getPathFromRoot(href)[REPLACE](base, '')
+}
+
+function emit(force) {
+  var path = getPathFromBase()
+  if (force || path != current) {
+    central[TRIGGER]('emit', path)
     current = path
   }
 }
 
-var route = function(arg) {
-  // string
-  if (arg[0]) {
-    loc.hash = arg
-    emit(arg)
+function click(e) {
+  if (
+    e.which != 1 // not left click
+    || e.metaKey || e.ctrlKey || e.shiftKey // or meta keys
+    || e.defaultPrevented // or default prevented
+  ) return
 
-  // function
-  } else {
-    fns.on('H', arg)
+  var el = e.target
+  while (el && el.nodeName != 'A') el = el.parentNode
+  if (
+    !el || el.nodeName != 'A' // not A tag
+    || el[HAS_ATTRIBUTE]('download') // has download attr
+    || !el[HAS_ATTRIBUTE]('href') // has no href attr
+    || el.target && el.target != '_self' // another window or frame
+    || el.href.indexOf(loc.href.match(RE_ORIGIN)[0]) == -1 // cross origin
+  ) return
+
+  if (el.href != loc.href) {
+    if (el.href.split('#')[0] == loc.href.split('#')[0]) return // internal jump
+    go(getPathFromBase(el.href), el.title || doc.title)
   }
+  e.preventDefault()
 }
 
 /**
- * Exec routing right now
- * @param {function} fn - your action
+ * Go to the path
+ * @param {string} path - destination path
+ * @param {string} title - page title
  */
-route.exec = function(fn) {
-  fn.apply(null, parser(hash()))
+function go(path, title) {
+  title = title || doc.title
+  // browsers ignores the second parameter `title`
+  history.pushState(null, title, base + path)
+  // so we need to set it manually
+  doc.title = title
+  emit()
+}
+
+/**
+ * Go to path or set action
+ * a single string:                go there
+ * two strings:                    go there with setting a title
+ * a single function:              set an action on the default route
+ * a string/RegExp and a function: set an action on the route
+ * @param {(string|function)} first - path / action / filter
+ * @param {(string|RegExp|function)} second - title / action
+ */
+prot.m = function(first, second) {
+  if (first[0] && (!second || second[0])) go(first, second)
+  else if (second) this.r(first, second)
+  else this.r('@', first)
+}
+
+/**
+ * Stop routing
+ */
+prot.s = function() {
+  this.off('*')
+  this.$ = []
+}
+
+/**
+ * Emit
+ * @param {string} path - path
+ */
+prot.e = function(path) {
+  this.$.concat('@').some(function(filter) {
+    var args = (filter == '@' ? parser : secondParser)(normalize(path), normalize(filter))
+    if (args) {
+      this[TRIGGER].apply(null, [filter].concat(args))
+      return true // exit from loop
+    }
+  }, this)
+}
+
+/**
+ * Register route
+ * @param {string} filter - filter for matching to url
+ * @param {function} action - action to register
+ */
+prot.r = function(filter, action) {
+  if (filter != '@') this.$.push(filter)
+  this.on(filter, action)
+}
+
+var mainRouter = new Router()
+var route = mainRouter.m.bind(mainRouter)
+
+/**
+ * Create a sub router
+ * @returns {function} the method of a new Router object
+ */
+route.create = function() {
+  var newSubRouter = new Router()
+  // stop only this sub-router
+  newSubRouter.m.stop = newSubRouter.s.bind(newSubRouter)
+  // return sub-router's main method
+  return newSubRouter.m.bind(newSubRouter)
+}
+
+/**
+ * Set the base of url
+ * @param {(str|RegExp)} arg - a new base or '#' or '#!'
+ */
+route.base = function(arg) {
+  base = arg || '#'
+  current = getPathFromBase() // recalculate current path
+}
+
+/** Exec routing right now **/
+route.exec = function() {
+  emit(true)
 }
 
 /**
  * Replace the default router to yours
  * @param {function} fn - your parser function
+ * @param {function} fn2 - your secondParser function
  */
-route.parser = function(fn) {
-  parser = fn
+route.parser = function(fn, fn2) {
+  if (!fn && !fn2) {
+    // reset parser for testing...
+    parser = DEFAULT_PARSER
+    secondParser = DEFAULT_SECOND_PARSER
+  }
+  if (fn) parser = fn
+  if (fn2) secondParser = fn2
+}
+
+/**
+ * Helper function to get url query as an object
+ * @returns {object} parsed query
+ */
+route.query = function() {
+  var q = {}
+  loc.href[REPLACE](/[?&](.+?)=([^&]*)/g, function(_, k, v) { q[k] = v })
+  return q
 }
 
 /** Stop routing **/
 route.stop = function () {
   if (started) {
-    win.removeEventListener(EVT, emit, false)
-    fns.off('*')
+    win[REMOVE_EVENT_LISTENER](POPSTATE, emit)
+    doc[REMOVE_EVENT_LISTENER](clickEvent, click)
+    central[TRIGGER]('stop')
     started = false
   }
 }
@@ -85,12 +248,15 @@ route.stop = function () {
 /** Start routing **/
 route.start = function () {
   if (!started) {
-    win.addEventListener(EVT, emit, false)
+    win[ADD_EVENT_LISTENER](POPSTATE, emit)
+    doc[ADD_EVENT_LISTENER](clickEvent, click)
     started = true
   }
 }
 
 /** Autostart the router **/
+route.base()
+route.parser()
 route.start()
 
 module.exports = route
