031bf631a1e0be018788147c74c59ce852ceca0f
Martin Sveden <msveden@paypal.com>
2017-03-03 14:04:34 +0100

    fix code style to convention

--- src/index.js
+++ src/index.js
@@ -326,16 +326,16 @@ route.stop = function () {
 route.start = function (autoExec) {
   if (!started) {
     if (win) {
-      var targetState = 'interactive';
+      var targetState = 'interactive'
       if (document.readyState === targetState) {
-          start(autoExec)
+        start(autoExec)
       }
       else {
         document.onreadystatechange = function () {
           if (document.readyState === targetState) {
             // the timeout is needed to solve
             // a weird safari bug https://github.com/riot/route/issues/33
-            setTimeout(function() { start(autoExec); }, 1);
+            setTimeout(function() { start(autoExec) }, 1)
           }
         }
       }
