93c04f394fd12e56ca1dc5f1429e75916d6d857c
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-12-31 00:58:11 +0100

    updated: expose all the rawth methods

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import route, { router } from 'rawth'
 import Route from './components/route-hoc.riot'
 import Router from './components/router-hoc.riot'
 import getCurrentRoute from './get-current-route'
 import initDomListeners from './dom'
 import setBase from './set-base'
 
+export * from 'rawth'
+
 export {
   getCurrentRoute,
   initDomListeners,
   setBase,
-  router,
-  route,
   Router,
   Route
 }
+
+export { default as route } from 'rawth' // eslint-disable-line
