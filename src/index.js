969163152a381de1cc02a71bd8a3965b34cb808d
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-12-31 01:02:56 +0100

    updated: improved module syntax

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+import route, {
+  createURLStreamPipe,
+  filterStrings,
+  match,
+  router,
+  toPath,
+  toRegexp,
+  toURL
+} from 'rawth'
 import Route from './components/route-hoc.riot'
 import Router from './components/router-hoc.riot'
 import getCurrentRoute from './get-current-route'
 import initDomListeners from './dom'
 import setBase from './set-base'
 
-export * from 'rawth'
-
 export {
+  route,
+  createURLStreamPipe,
+  filterStrings,
+  match,
+  router,
+  toPath,
+  toRegexp,
+  toURL,
   getCurrentRoute,
   initDomListeners,
   setBase,
   Router,
   Route
 }
-
-export { default as route } from 'rawth' // eslint-disable-line
