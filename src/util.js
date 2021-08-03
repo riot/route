ecedbd3ba1497696ef27fc631a26bd9b20d31eec
gianlucaguarini <gianluca.guarini@gmail.com>
2021-08-03 16:22:50 +0200

    updated: improve node isomorphic support

--- src/util.js
+++ src/util.js
@@ -1,3 +1,4 @@
+export const getGlobal = () => getWindow() || global
 export const getWindow = () => typeof window === 'undefined' ? null : window
 export const getDocument = () => typeof document === 'undefined' ? null : document
 export const getHistory = () => typeof history === 'undefined' ? null : history
@@ -5,3 +6,15 @@ export const getLocation = () => {
   const win = getWindow()
   return win ? win.location : {}
 }
+
+export const defer = (() => {
+  const globalScope = getGlobal()
+
+  return globalScope.requestAnimationFrame || globalScope.setTimeout
+})()
+
+export const cancelDefer = (() => {
+  const globalScope = getGlobal()
+
+  return globalScope.cancelAnimationFrame || globalScope.clearTimeout
+})()
