6d6aa0776b4e6e4323c7ec6162a557091ed1c331
Gianluca Guarini <gianluca.guarini@gmail.com>
2024-12-06 22:45:56 +0100

    fix: #177

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,8 @@ import { RiotComponentWrapper, RiotComponent } from 'riot'
 import { URLWithParams } from 'rawth'
 
 export * from 'rawth'
-export type Route = RiotComponentWrapper<
+
+export declare const Route: RiotComponentWrapper<
   RiotComponent<{
     path: string
     'on-before-mount'?: (path: URLWithParams) => void
@@ -11,13 +12,15 @@ export type Route = RiotComponentWrapper<
     'on-unmounted'?: (path: URLWithParams) => void
   }>
 >
-export type Router = RiotComponentWrapper<
+
+export declare const Router: RiotComponentWrapper<
   RiotComponent<{
     base?: string
     'initial-route'?: string
     'on-started'?: (route: string) => void
   }>
 >
+
 export declare function getCurrentRoute(): string
 export declare function initDomListeners(): void
 export declare function setBase(base: string): void
